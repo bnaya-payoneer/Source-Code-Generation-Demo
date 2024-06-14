b69129d38ccfa4c5deded9fced3a7fed09425c54
Bnaya Eshet <bnayaes@payoneer.com>
2024-06-14 15:44:43 +0300

    json perf no init

--- JsonGeneratorDemo/Entities/Fellow.cs
+++ JsonGeneratorDemo/Entities/Fellow.cs
@@ -10,16 +10,19 @@ public partial class Fellow
     #region public static Fellow Default { get;  } = ...
 
     public static Fellow Default { get; } = new Fellow
-    { 
+    {
         Id = 10,
         Name = "Marry",
         DateOfBirth = DateTimeOffset.UtcNow.AddYears(-50),
-        Addresses = Enumerable.Range(0, 10)
-                        .Select(i => new Address($"Type {i}",
-                                                 $"Country {i}",
-                                                 $"City {i}",
-                                                 $"Street {i}",
-                                                 i))
+        Locations = Enumerable.Range(0, 10)
+                        .Select(i => new Location
+                        {
+                            Type = $"Type {i}",
+                            Country = $"Country {i}",
+                            City = $"City {i}",
+                            Street = $"Street {i}",
+                            House = i
+                        })
                         .ToList()
     };
 
@@ -30,5 +33,5 @@ public partial class Fellow
     public DateTimeOffset DateOfBirth { get; set; }
 
     [OrderedEquality]
-    public List<Address> Addresses { get; set; }
+    public List<Location> Locations { get; set; }
 }
